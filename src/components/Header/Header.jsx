4642663e6233b660868f8c458ec4929f68f9ac33
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-05-01 21:54:19 -0300

    header mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
 
 export default function Header() {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggle = () => setIsOpen(!isOpen)
+
+
     return (
-        <div className='header-container'>
-            <img src='/logo-juanma.png' />
-            <div className='header-links'>
-                <Link to='/' className='link'>Home</Link>
-                <Link to='/About' className='link'>About</Link>
-                <Link to='/Projects' className='link'>Projects</Link>
-                <Link to='/Contact' className='link'>Contact</Link>
+        <div>
+            <div className='header-container'>
+                <div className='menu' onClick={toggle}>
+                    <img className='menu-logo' src='/menu-logo.png' onClick={toggle}></img>
+                </div>
+                <Link to='/' className='logo-juanma'>
+                    <img src='/logo-juanmaa.png' />
+                </Link>
+                <div className='header-links'>
+                    <Link to='/' className='link'>Home</Link>
+                    <Link to='/About' className='link'>About</Link>
+                    <Link to='/Projects' className='link'>Projects</Link>
+                    <Link to='/Contact' className='link'>Contact</Link>
+                </div>
             </div>
+
+            {
+                isOpen && <div className='menu-mobile'>
+                        <Link to='/' className='link'>Home</Link>
+                        <Link to='/About' className='link'>About</Link>
+                        <Link to='/Projects' className='link'>Projects</Link>
+                        <Link to='/Contact' className='link'>Contact</Link>
+                    {/* <img className='menu-logo' src='/menu-logo.png' onClick={toggle}></img> */}
+                </div>
+            }
         </div>
     )
 }
