acb168b168f5fa8d7eb9125b945bbf2be0025e32
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-05-06 21:42:57 -0300

    added new pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Projects from './Projects';
 
 export const router = (
         <Routes>
             <Route path="/" element={<MainLayout />}>
                 <Route path="/" element={<Home />} />
-                <Route path="About" element={<Home />} />
-                <Route path="Contact" element={<Home />} />
-                <Route path="Projects" element={<Home />} />
+                <Route path="About" element={<About />} />
+                <Route path="Contact" element={<Contact />} />
+                <Route path="Projects" element={<Projects />} />
             </Route>
         </Routes>
 );
