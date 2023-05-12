cdd5cfe8f683b225b154ce2991aa51f1d63e9a1c
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-05-12 18:29:10 -0300

    added section title

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,13 +4,16 @@ import Project from './Project'
 
 export default function Projects() {
     return (
-        <div className='projects'>
-            <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
-            <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
-            <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
-            <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
-            <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
+        <section className='projects-section'>
+            <h2>{`<Projects/>`}</h2>
+            <div className='projects'>
+                <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
+                <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
+                <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
+                <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
+                <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
 
-        </div>
+            </div>
+        </section>
     )
 }
