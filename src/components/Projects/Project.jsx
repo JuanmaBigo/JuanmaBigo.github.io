028d066066a0830ddf632a05901af7b11abe0094
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-05-13 23:50:37 -0300

    added hover styles

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+
 import './Projects.css'
+import webLogo from '/web-logo.png'
 
 export default function Project({ name, linkRepo, linkHost, img }) {
+    const card = useRef()
+    const [state, setState] = useState(false)
+    const [scale, setScale] = useState(1)
+    const [transition, setTransition] = useState(0.5)
+    const [linksDisplay, setLinksDisplay] = useState(0)
+
+
+    const handleMouseEnter = () => {
+        setState(true)
+        setScale(1.05)
+        setTransition(0.5)
+        setLinksDisplay(1)
+    }
+    const handleMouseLeave = () => {
+        setState(false)
+        setScale(1)
+        setTransition(0.5)
+        setLinksDisplay(0)
+    }
+
+    const styles = {
+        transform: `scale(${scale})`,
+        transition: `transform ${transition}s ease-out`
+    }
+
+    const stylesLinks = {
+        display: linksDisplay,
+        transform: `scale(${linksDisplay})`
+    }
+
     return (
-        <div className='project'>
+        <div className='project' ref={card} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={styles}>
             <img src={img} />
             <h3>{name}</h3>
-            <div className='project-link'>
-                <a href={linkRepo}>Repo</a>
-                <a href={linkHost}>Host</a>
+            <div className='project-link' style={stylesLinks}>
+                <a href={linkRepo}><img src='https://cdn.icon-icons.com/icons2/2429/PNG/512/github_logo_icon_147285.png' /><p>REPO</p></a>
+                <a href={linkHost}><img src={webLogo} /><p>LIVE</p></a>
             </div>
 
         </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import './Projects.css'
 import Project from './Project'
+import empireImg from '/projects/empire.png'
+import mingaImg from '/projects/minga.png'
+import amazingImg from '/projects/amazing-events.png'
+import petshopImg from '/projects/petshop.png'
+import weatherImg from '/projects/weather-app.png'
 
 export default function Projects() {
     return (
         <section className='projects-section'>
             <h2>{`<Projects/>`}</h2>
             <div className='projects'>
-                <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
-                <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
-                <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
-                <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
-                <Project name={'Project'} linkRepo={'linkrepo'} linkHost={'linkhost'} img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5fxfhww25ysyMkq_IELQsXvEtbPHIM_fHUQ&usqp=CAU'} />
-
+                <Project name={'Empire'} linkHost={'https://empire-project.netlify.app/'} linkRepo={'https://github.com/JuanmaBigo/Empire-Front'} img={empireImg} />
+                <Project name={'Minga (manga reader)'} linkHost={'https://minga-red.netlify.app/'} linkRepo={'https://github.com/JuanmaBigo/Minga-Front'} img={mingaImg} />
+                <Project name={'Amazing Events'} linkHost={'https://juanmabigo.github.io/Amazing-Events/'} linkRepo={'https://github.com/JuanmaBigo/Amazing-Events'} img={amazingImg} />
+                <Project name={'Patitas Petshop'} linkHost={'https://juanmabigo.github.io/PetShop/'} linkRepo={'https://github.com/JuanmaBigo/PetShop'} img={petshopImg} />
+                <Project name={'Weather App'} linkHost={'https://juanmabigo.github.io/weather-app/'} linkRepo={'https://github.com/JuanmaBigo/weather-app'} img={weatherImg} />
             </div>
         </section>
     )
