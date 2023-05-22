5f0ba870ac48a69ce15ad0ad6069c0e952b4fecd
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-05-21 22:29:53 -0300

    added footer and download cv button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import './Footer.css'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <div className='footer-container'>
+            <div className='social-container'>
+                <Link to='https://www.instagram.com/juanmabigo/' className='social-logo'>
+                    <img src='https://www.nicepng.com/png/full/52-520535_free-files-github-github-icon-png-white.png' />
+                </Link>
+                <Link to='https://www.instagram.com/juanmabigo/' className='social-logo'>
+                    <img src='https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/linkedin-icon-18-256.png' />
+                </Link>
+                <Link to='https://www.instagram.com/juanmabigo/' className='social-logo'>
+                    <img src='https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-white-glyph.png' />
+                </Link>
+            </div>
+            <p>
+                © 2021 Juan Mateo Bigoritto
+            </p>
+        </div>
     )
 }
