c1b28f552f2d7655ef24f9e97b4b8fcb7c26d458
JuanmaBigo <juanmacuentatanlet@gmail.com>
2023-06-04 22:33:01 -0300

    deleted unused files

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import Home from './Home/Home';
 import About from './About/About';
-import Projects from './Projects/Projects';
-// import ProjectDetails from './Projects/ProjectDetails';
+// import Projects from './Projects/Projects';
 
 export const router = (
         <Routes>
             <Route path="/" element={<MainLayout />}>
                 <Route path="/" element={<Home />} />
                 <Route path="About" element={<About />} />
-                <Route path="Projects" element={<Projects />} />
-                {/* <Route path="Projects/:name" element={<ProjectDetails />} /> */}
+                {/* <Route path="Projects" element={<Projects />} /> */}
             </Route>
         </Routes>
 );
